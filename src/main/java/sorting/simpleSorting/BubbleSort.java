acb1fd7e62a4d7bce4ea3793121541cff77e50bb
Francisco-xiq <francisco.pereira.silva.filho@ccc.ufcg.edu.br>
2023-08-11 11:15:19 -0300

    Commit 1

---
+++ src/main/java/sorting/simpleSorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package sorting.simpleSorting;
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * The bubble sort algorithm iterates over the array multiple times, pushing big
+ * elements to the right by swapping adjacent elements, until the array is
+ * sorted.
+ */
+public class BubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		boolean swapou = true;
+		int i = 0;
+		while(swapou){
+			for(int j = ++leftIndex; j <= rightIndex - i; j++){
+				if(array[i].compareTo(array[j]) > 0){
+					Util.swap(array, i, j);
+					swapou = true;
+					i = j;
+				
+				}
+			}
+		}
+	}
+}
+
+
